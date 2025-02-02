4d333b3bc58d94ff12d67a0746e85e1aca25b52f
Nikhil Raj <143124316+nikhil00035@users.noreply.github.com>
2025-02-02 23:29:53 +0530

    Time: 3 ms (46.31%) | Memory: 12 MB (5.26%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string original=to_string(x);
+        string reversed=string(original.rbegin(),original.rend());
+        return original==reversed;
+        
+    }
+};
